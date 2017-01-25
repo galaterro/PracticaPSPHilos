f6d5c5103f40e948e09826fd62aae1c0a381df7b
galaterro <adrix1992@gmail.com>
2017-01-25 21:08:47 +0100

    First exercise completed.

--- src/es/adrigala/ej1/ControladorHilos.java
+++ src/es/adrigala/ej1/ControladorHilos.java
@@ -6,29 +6,18 @@ import java.util.Random;
  * Created by juanxxiii on 23/01/2017.
  */
 public class ControladorHilos {
-    private boolean empezado;
+
+    private int control = 1;
 
     public ControladorHilos() {
-        this.empezado = false;
-    }
 
-    public boolean isEmpezado() {
-        return empezado;
-    }
-
-    public void empezar() {
-        this.empezado = true;
-    }
-
-    public void acabar(){
-        this.empezado = false;
     }
 
     public synchronized void correr(String nombre) throws InterruptedException {
-        while(this.isEmpezado()){
+        while(!nombre.equals("Corredor " + control)){
             wait();
         }
-        this.empezar();
+
         System.out.println("El hilo " + nombre + " ha empezado");
         try {
             Thread.sleep((long)new Random().nextInt(1000));
@@ -41,7 +30,7 @@ public class ControladorHilos {
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        this.acabar();
+        control++;
         notifyAll();
     }
 }
